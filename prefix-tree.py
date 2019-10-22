8b74cfc2e1b9cc735e18ddf9b0631923fb6ef92b
nnhattruong98 <nnhattruong30@gmail.com>
2019-10-22 21:02:13 +0700

    Create prefix tree

---
+++ prefix-tree.py
@@ -0,0 +1,46 @@
+class Node:
+    "Trie node class"
+    def __init__(self):
+        self.children = [None] * 26
+        self.isEndOfWord = False
+
+class Trie:
+    "Trie data structure class"
+    def __init__(self):
+        self.root = self.getNode()
+    
+    def getNode(self):
+        "Return new node (initialized to NULL)"
+        return Node()
+    
+    def _charToIndex(self, ch):
+        "Converts key current character into index"
+        return (ord(ch)- ord('a'))
+    
+    def insertNode(self, word):
+        "Insert key into trie"
+        pNode = self.root
+        length = len(word)
+        for level in range(length):
+            index = self._charToIndex(word[level])
+            if not pNode.children[index]:
+                pNode.children[index] = self.getNode()
+            pNode = pNode.children[index]
+        
+        pNode.isEndOfWord = True
+
+    def searchNode(self, word):
+        "Search key in the trie"
+        pNode = self.root
+        length = len(word)
+        
+        for level in range(length):
+            index = self._charToIndex(word[level])
+            if not pNode.children[index]:
+                return False
+            pNode = pNode.children[index]
+
+        return pNode != None and pNode.isEndOfWord == True
+
+if __name__ == "__main__":
+    pass
